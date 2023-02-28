2ce622335abfb23acfabae39b8e2f0e6436acf2a
kerimhejsalah <k.hejsalah@gmail.com>
2023-02-28 10:43:42 +0100

    first commit

---
+++ sfoew/assets/i18n/en.ts
@@ -0,0 +1,173 @@
+export const locale = {
+    lang:'en',
+    data:{
+        LANGUAGEOPTION:{
+            CAPTION:"Choose Language"
+        },
+        MENU:{
+            HOME:"Home",
+            CUSTOMERES:"Customers",
+            ITEM:"Item"
+        },
+        HOME:{
+            TITLE:"Multilingual Website",
+            SELECT:".SELECT"
+        },
+        PROFILE:{
+            NAME: "First and last name",
+            EMAIL:"Email",
+            TELE:"Phone",
+            PROF:"Professionnel",
+            PAT:"Patient",
+            PROFIL:"My profile",
+            BRITHDATE:"Birth date",
+            ADRESS:"Adress",
+            UPDATE:"Edit",
+            PHOTOPROFIL:"Edit photo",
+            SEND:"Send",
+            UPDATEPROFILE:"Edit profile",
+        },
+        LISTPATIENT:{
+            LISTPATIENT:"List of patients",
+            SEARCH:"Search",
+            STATUSFORMS:"Status of the form",
+            NAME:"Name",
+            PRENOM:"Last name",
+            DATECREATED:"Date created",
+            ETAT:"Status",
+            AUCUNN:"NO PATIENTS",
+            PATIENT:"My patients",
+            CC:"Create patient account"
+        },
+        FORMS:{
+            FORMS:"Forms",
+            DO:"Folders",
+            LISTPATIENT:"List of forms",
+            VIEWFORMS:"View the form",
+            AFFECTFORM:"Affect a form"
+        },
+        HOMES:{
+            HOME:"Home",
+            APROP:"About",
+            CONTACT:"Contact",
+            ESPACEPRO:"Professional space",
+            ESPACEPATIENT:"Patient space",
+            PHRASE:"WELCOME TO THE SCORES APP",
+            PHRASE1:"Our team is here to help you",
+            START:"START",
+            INFO:"More information",
+            CONT:"Contact us"
+        },
+        AUCUN :{
+            AUCUNN:"NO INVITATION"
+        },
+        INVIT:{
+            DETAIL:"Detail",
+            ACCEPT:"Accept",
+            INVIT:"My Invitations"
+        },
+        LISTCONTACT:{
+            LISTPATIENT:"List of contacts",
+            SEARCH:"Search",
+            STATUSFORMS:"Status of the form",
+            NAME:"Name",
+            PRENOM:"Last name",
+            DATECREATED:"Date created",
+            ETAT:"Status"
+        },
+        SIDEMENU:{
+         MC:"My account",
+         LP:"Patient list",
+         MP:"My Patients",
+         MF:"My forms",
+         DEC:"Disconnection",
+         LI:"Invitation List",
+         CO:"Contacts"   ,
+         DO:"Folders",
+         AP:"Add patient"
+        },
+        AFFECTFORMS:{
+            AFFECTBY:"Form affect by ",
+            FORMSREM:"Form completed by",
+            TITLE:"Title",
+            DESC:"Description",
+            AUCUNN:"AUCUNE CONTACT"
+        },
+        CONTACT :{
+            LOCATION:"Adress",
+            EMAIL:"Email",
+            TELE:"Phone",
+            YN:"Name and LastName",
+            YE:"Email",
+            SB:"Subject",
+            MES:"Message",
+            CONT:"CONTACT US AT",
+            SEND:"Send Message"
+        },
+        AUTH:{
+            IDENTIFIER:"Identify / Register",
+            IDENTIFIER2:"Identify",
+            INSCRIRE :"Register",
+            EMAIL :"Email",
+            MOTDEPASS:"Password",
+            SIGNIN:"Sign In",
+            CG:"Connection with Gouugle",
+            RM:"Remember me !",
+            NOM:"Name",
+            PRENOM:"Last Name",
+            DATEN:"Date of birth",
+            TELE:"Phone Number",
+            ADDRESS:"Adress",
+            METIER:"Trade",
+            CFM:"Confirm the password ",
+            ACCEPTT:"Accept Terms & Conditions",
+            PROF:"Professional  ",
+            PAT:"Patient ",
+            POID:"Weight (in kg)",
+            SIZE:"Size (in cm)",
+            IMC:"IMC",
+            SEXEH:"Male",
+            SEXEF:"Female",
+            TITRE:"Title" ,
+            CS:" Confirm Signature ",
+            CLEAR:"Clear",
+            ENR:"Register",
+            MM:"MM/DD/YYYY",
+            GENDER:"Gender"
+        },
+        P:{
+            MC:"GETTING TO KNOW US",
+          PH1:"Application Scores and Scientifically Referenced Self-Assessment Scales for Health Professionals ",
+          PH2:"Our company has developed with a cohort of expert patients and practitioners a connected self-assessment medical device to meet your expectations. We offer a modular application that adapts to your practice, which involves your patient in the management and evolution of his care. ",
+          PH3:"The results are available instantly, an analysis possible from the season by the patient(s) for a more rapid follow-up of his treatment ."
+        },
+        PAGINATION:{
+            PR:"Previous",
+            NEXT:"Next"
+        },
+        VALIDEMAIL:{
+           EA:"Email address",
+           ENTERE:"Enter email",
+           VALIDER:"Valid"
+        },
+        DIALOG:{
+            BN:"Hi",
+            PARA:`To help me develop a treatment that is best suited to your symptoms, please complete the following questionnaires by clicking on the appropriate button(s). These questions are about certain pelvic symptoms. When answering, consider the symptoms you experienced. You may object at any time without further justification. The information collected is strictly confidential. They will be the subject, unless you object justified, of a computer registration reserved for the use of your health professional. Your attending health professional is available to provide you with any information and any information necessary to your medical condition. Any doctor, appointed by you, can also examine your entire file. No information will be distributed to third parties (Law No. 78-17 of 06/01/1978 as amended) .`,
+            PARAS:"I, the undersigned, ",
+            PARA1:`Declares that they freely and knowingly agree to complete the score forms and/or self-assessment scales, as part of a medical (or paramedical) consultation related to the pathology(s) for which I am consulting.
+            Commitment of the health professional: as a practitioner, he undertakes to have these questionnaires filled out according to ethical and ethical provisions and to ensure the confidentiality of the information collected.  
+            Patient’s freedom: consent can be withdrawn at any time without giving a reason and without incurring any responsibility or consequences. The answers to the questions are optional, the failure to answer does not have any consequences for the patient. 
+            Patient information: the patient has the possibility to obtain additional information about the questionnaires from the health professional. 
+            Confidentiality of information: all patient information will be kept anonymous and confidential.
+            Ethics and ethics: The health professional is committed to maintaining confidentiality and professional secrecy for all information concerning the patient .`
+        },
+        UPDATEMOT:{
+            MP:"Modify Password",
+            SEND:"Send",
+            P:"Verification link has been sent to your mailbox ",
+            MO:"Forgot password ?"
+         }
+    }     
+
+    
+}
